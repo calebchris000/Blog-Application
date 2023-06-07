ec8003e82a7cefd6474c1107c2f36092a714ebc4
Caleb Nwaizu <calebchris000@gmail.com>
2023-06-07 21:14:22 +0100

    Models create, implement methods

---
+++ app/models/author.rb
@@ -0,0 +1,10 @@
+class Author < ApplicationRecord
+    has_many :post
+    has_many :comment
+    has_many :like
+    
+
+    def last_3_post
+        post.order(created_at: :desc).limit(3)
+      end
+end
---
+++ app/models/like.rb
@@ -0,0 +1,9 @@
+class Like < ApplicationRecord
+    belongs_to :post
+    belongs_to :author
+    validates :likes_counter, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+
+    def increment_like_count
+    post.increment!(:likes_counter)
+    end
+end
