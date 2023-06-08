f64cdb06ad7d03797a4d8bf3f2930b48b57fb098
Caleb Nwaizu <calebchris000@gmail.com>
2023-06-08 18:37:11 +0100

    Add validations

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,6 +2,9 @@ class Author < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
+  validate :name, presence :true
+  validates :post_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
 
   def last_3_post
     post.order(created_at: :desc).limit(3)
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,9 +4,9 @@ class Post < ApplicationRecord
   has_many :comments, counter_cache: :post_counter
   belongs_to :author
   has_many :likes
-  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :comment_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :title, presence: true, length: { maximum: 250 }
+  validates :comment_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def top_5_comment
     comments.order(created_at: :desc).limit(5)
