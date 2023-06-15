f1778d1016370c7f677e56ee00a2ca0f8392c21c
Caleb Nwaizu <calebchris000@gmail.com>
2023-06-15 14:04:23 +0100

    Add views and connect view to db

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,9 +1,16 @@
 class AuthorsController < ActionController::Base
   def index
     # Where the index logic goes
+    @authors = Author.all
+    @author_posts = Post.all
   end
-
+  
   def show
+    @current_user_id = params[:id].to_i
+    @current_user = Author.find(@current_user_id)
+    @total_posts = Post.where(author_id: @current_user.id)
+    @current_user_bio = @current_user.bio
+
     # Where the show logic goes
   end
 end
